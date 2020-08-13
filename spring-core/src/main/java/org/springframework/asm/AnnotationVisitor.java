3c036acbf3185b926a79043a73688dbfc49bf5db
zhangll6 <zhangll6@ziroom.com>
2020-08-13 20:58:37 +0800

    @Component 注释

--- spring-core/src/main/java/org/springframework/asm/AnnotationVisitor.java
+++ spring-core/src/main/java/org/springframework/asm/AnnotationVisitor.java
@@ -1,7 +1,6 @@
 // ASM: a very small and fast Java bytecode manipulation framework
 // Copyright (c) 2000-2011 INRIA, France Telecom
 // All rights reserved.
-//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions
 // are met:
